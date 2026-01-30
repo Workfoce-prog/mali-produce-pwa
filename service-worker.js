e6c681f4d0a9bcaab09733f796b4fd8f5280e925
Workfoce-prog <ziyackcoulibaly@gmail.com>
2026-01-30 17:50:41 -0600

    Add service worker for asset caching and offline access
    Implement a service worker for caching assets and offline support.

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE = "mali-produce-pwa-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./offline.html", "./icons/icon-192.png", "./icons/icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).catch(() => caches.match("./offline.html")))
+  );
+});
